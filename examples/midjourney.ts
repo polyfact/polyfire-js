80131472a46379817e267a4249e8bbd8adaa0b1d
kgricour <kevin@newton.so>
2023-08-10 15:59:18 +0200

    🔧 refactor: remove unnecessary system prompt

--- examples/midjourney.ts
+++ examples/midjourney.ts
@@ -1,7 +1,5 @@
 import { generate } from "../lib/index";
 
-const systemPrompt = `Add all numbers together which are given in an array. your response have to be like : "4" or "2"`;
-
 (async () => {
     const res = await generate(
         "i want a Swiss white beger dog",
@@ -11,7 +9,7 @@ const systemPrompt = `Add all numbers together which are given in an array. your
         },
         {
             endpoint: "http://localhost:8080",
-            token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjJ9.5ThO9HHg9bxqpnoYUJmeimifVwzcLQ_DsGNdll4q-Rc",
+            token: "<token>",
         },
     );
     console.log(res);
