3f569431f4364417a9dda83613e74d3308545149
Lancelot Owczarczak <lancelot@owczarczak.fr>
2023-08-31 16:57:04 +0200

    :wastebasket: Remove pdf-parse

--- lib/dataloader/index.ts
+++ lib/dataloader/index.ts
@@ -1,5 +1,3 @@
-import pdf from "pdf-parse";
-
 import { Memory } from "../memory";
 import { transcribe } from "../transcribe";
 import { splitString } from "../split";
@@ -27,33 +25,13 @@ async function loaderInputToBuffer(input: LoaderFileInput): Promise<Buffer> {
         return input;
     }
     if (typeof input === "string") {
-        const fs = await import("fs");
+        const fs = require("fs"); // eslint-disable-line
         return new Promise<Buffer>((res, rej) =>
-            fs.readFile(input, (err, data) => (err !== null ? rej(err) : res(data))),
+            fs.readFile(input, (err: any, data: any) => (err !== null ? rej(err) : res(data))),
         );
     }
 
     return stream2buffer(input);
-}
-
-async function pdfParsePages(fileBuffer: Buffer): Promise<string[]> {
-    return new Promise((res, rej) => {
-        const result: string[] = [];
-        pdf(fileBuffer, {
-            pagerender: (e) => {
-                return e
-                    .getTextContent({ normalizeWhitespace: true })
-                    .then(
-                        (e: { items: { str: string }[] }) =>
-                            e.items
-                                .map((s) => s.str)
-                                .join("")
-                                .replace(/\x00/g, ""), // eslint-disable-line
-                    )
-                    .then((r: string) => result.push(r));
-            },
-        }).then(() => res(result));
-    });
 }
 
 async function batchify<T extends Array<unknown>>(
@@ -69,22 +47,6 @@ async function batchify<T extends Array<unknown>>(
 }
 
 export type LoaderFunction = (memory: Memory, clientOptions: InputClientOptions) => Promise<void>;
-
-export function PDFLoader(file: LoaderFileInput): LoaderFunction {
-    return async function loadPdfIntoMemory(
-        memory: Memory,
-        _clientOptions: InputClientOptions = {},
-    ) {
-        const fileBuffer = await loaderInputToBuffer(file);
-        const allPages = await pdfParsePages(fileBuffer);
-
-        async function addPagesIntoMemory(pages: string[]) {
-            await Promise.all(pages.map((page) => memory.add(page)));
-        }
-
-        await batchify(allPages, 10, addPagesIntoMemory);
-    };
-}
 
 export function TextFileLoader(file: LoaderFileInput, maxTokenPerChunk = 100): LoaderFunction {
     return async function loadPdfIntoMemory(
