10c179834f7959135e6ef9be33b83b1395f4b95c
kev <kgricour@student.42.fr>
2023-09-04 15:58:26 +0200

    Update cmd/prompt/index.ts
    Co-authored-by: Lancelot Owczarczak <lancelot@owczarczak.fr>

--- cmd/prompt/index.ts
+++ cmd/prompt/index.ts
@@ -95,7 +95,7 @@ async function managePrompts(token: string): Promise<boolean> {
                 {
                     type: "input",
                     name: "name",
-                    message: "Update prompt name (leave blank to skip):",
+                    message: "Update the prompt's name (leave it blank to skip):",
                 },
                 {
                     type: "input",
