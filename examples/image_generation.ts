e3ae9cfb700a846f235478487d811fa1a402caac
Lancelot Owczarczak <lancelot@owczarczak.fr>
2023-09-07 18:45:53 +0200

    :recycle: Remove midjourney

--- examples/image_generation.ts
+++ examples/image_generation.ts
@@ -1,11 +1,7 @@
 import { generateImage } from "../lib/index";
 
 async function main() {
-    console.log(
-        await generateImage("Ash playing pokemon on the gameboy --niji", {
-            provider: "midjourney",
-        }),
-    );
+    console.log(await generateImage("Ash playing pokemon on the gameboy"));
 }
 
 main();
