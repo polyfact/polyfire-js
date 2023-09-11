363e3eedaa6c54a62490c082751f14176d3a8853
Lancelot Owczarczak <lancelot@owczarczak.fr>
2023-09-11 14:43:07 +0200

    :bug: Fix generate return/params type

--- lib/index.ts
+++ lib/index.ts
@@ -12,8 +12,6 @@ import { splitString, tokenCount } from "./split";
 import { usage } from "./user";
 import { get as KVGet, set as KVSet } from "./kv";
 import Polyfact, { PolyfactClientBuilder } from "./client";
-import usePolyfact from "./hooks/usePolyfact";
-import useChat from "./hooks/useChat";
 import { generateImage } from "./image";
 import { TextFileLoader, StringLoader, AudioLoader } from "./dataloader";
 
