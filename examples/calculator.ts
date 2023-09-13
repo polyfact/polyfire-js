afbcd06dae444ff2f7c46f184b3798a6e0a73ae4
Lancelot Owczarczak <lancelot@owczarczak.fr>
2023-09-13 16:19:25 +0200

    :recycle: Merge generateWith* functions with generate (same for Chat.sendMessage)

--- examples/calculator.ts
+++ examples/calculator.ts
@@ -1,12 +1,10 @@
-import { generate, generateWithTokenUsage, getAllPrompts } from "../lib/index";
+import { generate } from "../lib/index";
 
 const systemPrompt = `Add all numbers together which are given in an array. your response have to be like : "4" or "2"`;
 
 (async () => {
     const res = await generate("[7, 8, 9]", {
         systemPrompt,
-        infos: false,
-        web: true,
     });
     console.log(res);
 })();
