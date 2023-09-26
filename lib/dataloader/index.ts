36b593a5c61585f1f5c8d89c94b26d5a80a95378
Lancelot Owczarczak <lancelot@owczarczak.fr>
2023-09-26 10:29:36 +0200

    :sparkles: Add PdfLoader (#74)

--- lib/dataloader/index.ts
+++ lib/dataloader/index.ts
@@ -1,3 +1,4 @@
+import { getDocument, GlobalWorkerOptions, version } from "pdfjs-dist";
 import { Memory } from "../memory";
 import { transcribe } from "../transcribe";
 import { splitString } from "../split";
@@ -20,24 +21,8 @@ async function batchify<T extends Array<unknown>>(
 
 export type LoaderFunction = (memory: Memory, clientOptions: InputClientOptions) => Promise<void>;
 
-export function TextFileLoader(file: FileInput, maxTokenPerChunk = 100): LoaderFunction {
-    return async function loadPdfIntoMemory(
-        memory: Memory,
-        _clientOptions: InputClientOptions = {},
-    ) {
-        const fileBuffer = await fileInputToBuffer(file);
-        const splittedFile = splitString(fileBuffer.toString("utf8"), maxTokenPerChunk);
-
-        async function addBatchIntoMemory(batches: string[]) {
-            await Promise.all(batches.map(async (batch) => memory.add(batch)));
-        }
-
-        await batchify(splittedFile, 10, addBatchIntoMemory);
-    };
-}
-
 export function StringLoader(str: string, maxTokenPerChunk = 100): LoaderFunction {
-    return async function loadPdfIntoMemory(
+    return async function loadStringIntoMemory(
         memory: Memory,
         _clientOptions: InputClientOptions = {},
     ) {
@@ -51,6 +36,13 @@ export function StringLoader(str: string, maxTokenPerChunk = 100): LoaderFunctio
     };
 }
 
+export function TextFileLoader(file: FileInput, maxTokenPerChunk = 100): LoaderFunction {
+    return async function loadTextIntoMemory(...args) {
+        const fileBuffer = await fileInputToBuffer(file);
+        return StringLoader(fileBuffer.toString("utf8"), maxTokenPerChunk)(...args);
+    };
+}
+
 export function AudioLoader(file: FileInput, maxTokenPerChunk = 100): LoaderFunction {
     return async function loadAudioIntoMemory(
         memory: Memory,
@@ -58,13 +50,44 @@ export function AudioLoader(file: FileInput, maxTokenPerChunk = 100): LoaderFunc
     ) {
         const fileBuffer = await fileInputToBuffer(file);
         const transcription = await transcribe(fileBuffer, clientOptions);
-        const transcriptions = splitString(transcription, maxTokenPerChunk);
+        return StringLoader(transcription, maxTokenPerChunk)(memory, clientOptions);
+    };
+}
 
-        async function addBatchIntoMemory(batches: string[]) {
-            await Promise.all(batches.map(async (batch) => memory.add(batch)));
-        }
+export async function pdfToString(pdf: Uint8Array): Promise<string> {
+    if (typeof window === "undefined") {
+        GlobalWorkerOptions.workerSrc = `pdfjs-dist/build/pdf.worker.js`;
+    } else {
+        GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${version}/pdf.worker.js`;
+    }
+    const pdfDocument = await getDocument(pdf).promise;
+    const pagesPromises = [];
 
-        await batchify(transcriptions, 10, addBatchIntoMemory);
+    for (let i = 1; i <= pdfDocument.numPages; i++) {
+        pagesPromises.push(pdfDocument.getPage(i));
+    }
+
+    const pages = await Promise.all(pagesPromises);
+
+    const textEntries = await Promise.all(
+        pages.map(async (page) => {
+            const pageObject = await page.getTextContent();
+
+            return pageObject.items
+                .map((e) => ("str" in e ? e.str : ""))
+                .filter((e) => e !== "")
+                .join("\n");
+        }),
+    );
+
+    return textEntries.join("\n");
+}
+
+export function PdfLoader(file: FileInput, maxTokenPerChunk = 100): LoaderFunction {
+    return async function loadPdfIntoMemory(...args) {
+        const fileBuffer = await fileInputToBuffer(file);
+        const pdfText = await pdfToString(new Uint8Array(fileBuffer));
+        return StringLoader(pdfText, maxTokenPerChunk)(...args);
     };
 }
 
