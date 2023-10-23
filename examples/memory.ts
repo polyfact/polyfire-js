052cf4d95d2cdecede9e0464d7c34a9d2e72e6df
Lancelot Owczarczak <lancelot@owczarczak.fr>
2023-10-23 16:58:56 +0200

    :recycle: Add proper examples (#92)
    * :wastebasket: Delete all current examples
    
    * :books: Add hello-world-haiku example
    
    * :books: Add chatgpt clone example
    
    * :wastebasket: Remove auto-generated README.md
    
    * :books: Add warning to change project slug in chat example

--- examples/memory.ts
+++
@@ -1,6 +0,0 @@
-import { Memory } from "../lib/index";
-
-(async () => {
-    const memory = new Memory({ public: true }); // Create a public memory (default) - false for private
-    console.log("Created memory:", await memory.getId());
-})();
