790edef9db7c39a4fb4cfb3e71add9f972fca9de
Lancelot Owczarczak <lancelot@owczarczak.fr>
2023-10-23 15:53:42 +0200

    :wastebasket: Delete all current examples

--- examples/image_generation.ts
+++
@@ -1,7 +0,0 @@
-import { generateImage } from "../lib/index";
-
-async function main() {
-    console.log(await generateImage("Ash playing pokemon on the gameboy"));
-}
-
-main();
--- examples/updateMemory.ts
+++
@@ -1,6 +0,0 @@
-import { updateMemory } from "../lib/index";
-
-(async () => {
-    const result = await updateMemory("<memory-id>", "<input-data>");
-    console.log(result); // Outputs: { success: true }
-})();
