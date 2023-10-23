227fcd4a0801608b8cf4f87a0d6f25bf33feabe7
Lancelot Owczarczak <lancelot@owczarczak.fr>
2023-10-23 16:51:33 +0200

    :books: Add warning to change project slug in chat example

--- examples/chatgpt-clone/src/App.tsx
+++ examples/chatgpt-clone/src/App.tsx
@@ -11,7 +11,20 @@ function App() {
     return (
         <>
             {status === "unauthenticated" ? (
-                <button onClick={() => login("github")}>Login with Github</button>
+                <div>
+                    <h1 className="font-bold">
+                        Don't forget to change the project slug in src/index.tsx
+                    </h1>
+                    <p>
+                        After that you will be able to{" "}
+                        <button
+                            className="cursor-pointer bg-black text-white ml-1 p-1 px-5 font-mono-font-bold"
+                            onClick={() => login("github")}
+                        >
+                            Login With GitHub
+                        </button>
+                    </p>
+                </div>
             ) : status === "authenticated" ? (
                 <ChatBox
                     messages={messages?.slice().reverse() || []}
