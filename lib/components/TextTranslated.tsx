b5766adf3852db77450627ff173dcb127e7637bf
Lancelot Owczarczak <lancelot@owczarczak.fr>
2023-12-08 17:48:41 +0100

    :sparkles: Add TextTranslated react component (#117)
    * :sparkles: react/components: Add ImageGenerated Component
    
    * :zap: models: Add model in generateImage, removed provider
    
    * :zap: react/components: Add model in ImageGenerated
    
    * :zap: data: Add pagination + orderBy to Chat.getMessages()
    
    * :zap: react/components: Add TextTranslated component

---
+++ lib/components/TextTranslated.tsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import { usePolyfire } from "../hooks";
+
+export interface TextTranslatedProps extends React.HTMLAttributes<HTMLDivElement> {
+    text: string;
+    language: string;
+    loadingElement?: React.JSX.Element | string;
+}
+
+export function TextTranslated({
+    text,
+    language,
+    loadingElement,
+    ...props
+}: TextTranslatedProps): React.ReactElement {
+    const {
+        auth: { status },
+        models: { generate },
+    } = usePolyfire();
+
+    const [translation, setTranslation] = useState<string>();
+
+    useEffect(() => {
+        setTranslation(undefined);
+
+        if (status === "authenticated" && text) {
+            generate(text, {
+                systemPrompt: `Translate the text the user provided to this language: "${language}". Just say the translated text. Do not write anything else, do not write any explanation. Only write in the target language (${language})`,
+            }).then(setTranslation);
+        }
+    }, [status, generate, text]);
+
+    if (translation) {
+        return <div {...props}>{translation}</div>;
+    }
+    return <div {...props}>{loadingElement}</div>;
+}
