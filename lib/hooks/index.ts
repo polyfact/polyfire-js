7f4c6b0c177b0769d58a15a14f5e53772ea7d6c2
kgricour <kevin@newton.so>
2024-01-22 17:11:16 +0100

    chore: add export useVoiceToText

--- lib/hooks/index.ts
+++ lib/hooks/index.ts
@@ -1,5 +1,15 @@
 import usePolyfire, { PolyfireProvider } from "./usePolyfire";
 import useChat from "./useChat";
 import useAgent, { ActionAgent, Agent, DefinitionAction } from "./useAgent";
+import useVoiceToText from "./useVoiceToText";
 
-export { usePolyfire, PolyfireProvider, useChat, useAgent, ActionAgent, Agent, DefinitionAction };
+export {
+    usePolyfire,
+    PolyfireProvider,
+    useChat,
+    useAgent,
+    useVoiceToText,
+    ActionAgent,
+    Agent,
+    DefinitionAction,
+};
